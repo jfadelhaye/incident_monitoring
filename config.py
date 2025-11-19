d625c3e3d7a1c90a7a34882d24c9368c8987b204
Julien Delhaye <julien.delhaye@portainer.io>
2025-11-19 03:19:20 +0100

    add linear and notion

--- config.py
+++ config.py
@@ -13,11 +13,21 @@ FEEDS =  [
         "name": "Cloudflare",
         "url": "https://www.cloudflarestatus.com/history.rss",
     },
+    {
+        "name": "Linear",
+        "url": "https://linearstatus.com/feed.rss",
+    },
+    {
+        "name": "Notion",
+        "url": "https://www.notion-status.com/history.rss",
+    }
 ]
 
 SOURCE_COLORS = {
     "GitHub": "#24292e",
     "Docker": "#0db7ed",
     "Cloudflare": "#f38020",
+    "Linear": "#717ce1",
+    "Notion": "#6f6f6f",
 }
 
